a55cdc93eb726c60437e313ab6bdc2d9f834a5ce
tonio8mario <159304581+tonio8mario@users.noreply.github.com>
2024-11-01 14:51:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+function darkmode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+  }
+
+  let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+const abrirmodal = document.querySelector("#open-modal");
+
+const fecharmodal = document.querySelector("#fechar");
+
+const modal = document.querySelector("#modal");
+
+const fade = document.querySelector("#modal-fade");
+
+
+
+
+function abrirFechar(){
+
+   //fade.classList.toggle("show")
+
+    modal.showModal()
+
+  // modal.classList.remove("hide")
+
+   //fade.classList.remove("hide")
+
+}
+
+fecharmodal.addEventListener("click", () => {
+  modal.close();})
+
+
+/*fade.addEventListener("click",()=>{
+
+      fade.classList.toggle("show")
+
+    modal.classList.toggle("show")
+
+  //  modal.classList.add("hide")
+
+ //  fade.classList.add("hide")
+
+
+
+
+})*/
